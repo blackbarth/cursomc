9e266494a25e88cacfa609755e89a406ef18c996
blackbarth <blackbarth@outlook.com>
2019-04-16 00:24:25 -0300

    Produtos associassao muitos pra muitos

---
+++ src/main/java/br/com/luisaugustoferreira/cursomc/repositories/ProdutoRepository.java
@@ -0,0 +1,11 @@
+package br.com.luisaugustoferreira.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.luisaugustoferreira.cursomc.domain.Produto;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Integer>{
+
+}
